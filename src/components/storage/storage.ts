8685b1536b9fbc082311bca19e943d49f8732577
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-12-18 23:16:24 +0300

    feat: add controller

--- src/components/storage/storage.ts
+++ src/components/storage/storage.ts
@@ -1,11 +1,13 @@
-
 import { IProducts } from '../../types/product.interface';
+import Controller from '../controller/controller';
 
 class StorageData {
+  controller: Controller = new Controller();
   createStateData(data: IProducts) {
     const values = data?.products ? data.products : [];
     localStorage.dataProducts = JSON.stringify(values);
     console.log('put data products in localStorage');
+    this.controller.startAction();
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,3 @@ import { IProduct } from './types/product.interface';
 console.log('we are start!');
 const app = new App();
 app.start();
-
-//JSON.parse(localStorage.dataProducts).forEach((x: IProduct,index:number) => console.log(index+" - " + x.title));
