a9d2b22e019c48b55f4303c550743db168b366e9
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-12-18 23:31:54 +0300

    refactor: del test button

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,13 +1,13 @@
-import { IProduct } from "../../types/product.interface";
+import { IProduct } from '../../types/product.interface';
 
 class Controller {
   startAction() {
     console.log('start action');
-    let bat = document.querySelector('.btn') as HTMLElement;
-    bat.onclick = function () {
-      console.log('btn click');
-      JSON.parse(localStorage.dataProducts).forEach((x: IProduct,index:number) => console.log(index+" - " + x.title));
-    };
+    // let bat = document.querySelector('.btn') as HTMLElement;
+    // bat.onclick = function () {
+    //   console.log('btn click');
+    //   JSON.parse(localStorage.dataProducts).forEach((x: IProduct,index:number) => console.log(index+" - " + x.title));
+    // };
   }
 }
 
