e29b139e52b98c9695ce6aa7bd7f532b8228a4f3
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-12-20 00:26:19 +0300

    refactor: interface fields

--- src/types/product.interface.ts
+++ src/types/product.interface.ts
@@ -1,22 +1,28 @@
 export interface Product {
-    id: string;
-    tittle: string;
+  id: string;
+  tittle: string;
 }
 export interface Products {
-    filter(arg0: (_item: string, idx: number) => boolean): unknown;
-    length: number;
-    status: string;
-    totalResults: number;
-    products: Product[];
+  filter(arg0: (_item: string, idx: number) => boolean): unknown;
+  length: number;
+  status: string;
+  totalResults: number;
+  products: Product[];
 }
 
 export interface Product {
-    id: string;
-    title: string;
-    images:string[];
-    category:string;
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  brand: string;
+  thumbnail: string;
+  images: string[];
+  category: string;
 }
 export interface ProductSource {
-    id: null | string;
-    title: null | string;
+  id: null | string;
+  title: null | string;
 }
