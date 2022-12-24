ed38b60bf8087a90deafb1863facfb1f52952093
vigo44 <vvigo@rambler.ru>
2022-12-24 14:23:27 +0400

    feat: add routing in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from './components/app/app';
 import './styles.scss';
 import { IProduct } from './types/product.interface';
+import initRouter from './components/router/router';
 console.log('we are start!');
 const app = new App();
 app.start();
+initRouter();
