254cc67e9fa1c8b2b554b7fe357f74c9a9c525f7
vigo44 <vvigo@rambler.ru>
2023-01-06 03:01:44 +0400

    feat: add class filter

--- src/types/product.interface.ts
+++ src/types/product.interface.ts
@@ -1,22 +1,42 @@
 export interface IProduct {
-    id: string;
-    tittle: string;
+  id: string;
+  tittle: string;
 }
 export interface IProducts {
-    filter(arg0: (_item: string, idx: number) => boolean): unknown;
-    length: number;
-    status: string;
-    totalResults: number;
-    products: IProduct[];
+  filter(arg0: (_item: string, idx: number) => boolean): unknown;
+  length: number;
+  status: string;
+  totalResults: number;
+  products: IProduct[];
 }
 
 export interface IProduct {
-    id: string;
-    title: string;
-    images:string[];
-    category:string;
+  id: string;
+  title: string;
+  images: string[];
+  category: string;
 }
 export interface IProductSource {
-    id: null | string;
-    title: null | string;
+  id: null | string;
+  title: null | string;
+}
+
+export interface IFilterCheckbox {
+  name: string;
+  current: number;
+  max: number;
+  checked?: boolean;
+}
+export interface IFilterSlider {
+  currentMin: number;
+  currentMax: number;
+  min: number;
+  max: number;
+}
+
+export interface IProductFilter {
+  category: IFilterCheckbox[];
+  brand: IFilterCheckbox[];
+  price: IFilterSlider;
+  stock: IFilterSlider;
 }
