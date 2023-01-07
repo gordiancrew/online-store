5abe34992233e1d7667614ffb81edd19bfecabb5
vigo44 <vvigo@rambler.ru>
2023-01-08 01:53:48 +0400

    fix: start routing

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,8 +1,10 @@
 import { IProduct } from '../../types/product.interface';
+import initRouter from '../../components/router/router';
 
 class Controller {
   startAction() {
     console.log('start action');
+    initRouter();
     // let bat = document.querySelector('.btn') as HTMLElement;
     // bat.onclick = function () {
     //   console.log('btn click');
